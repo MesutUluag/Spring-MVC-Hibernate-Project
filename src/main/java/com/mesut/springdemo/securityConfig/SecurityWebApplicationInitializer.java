a6f4347ef675ea737944df0192987a73797b6ddd
Mesut Uluag <punk7119@gmail.com>
2020-11-21 10:03:21 +0300

    add spring security login logout with inMemoryAuthentication with default login page

---
+++ src/main/java/com/mesut/springdemo/securityConfig/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.mesut.springdemo.securityConfig;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+
+}
