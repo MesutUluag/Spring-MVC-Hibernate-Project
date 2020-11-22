d7323c9d1c7533e515b744a5cfcc511268132d70
Mesut Uluag <punk7119@gmail.com>
2020-11-22 17:17:25 +0300

    add rest api with pathvariable

--- src/main/java/com/mesut/springdemo/restControllers/StudentRestController.java
+++ src/main/java/com/mesut/springdemo/restControllers/StudentRestController.java
@@ -1,18 +1,23 @@
 package com.mesut.springdemo.restControllers;
 
 import com.mesut.springdemo.entity.Student;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    @RequestMapping("/students")
-    public List<Student> getAllStudents(){
-        List<Student> students= new ArrayList<>();
+
+    List<Student> students;
+
+    @PostConstruct //called after spring initialization
+    public void loadData(){
+        students= new ArrayList<>();
 
         Student student= new Student();
         student.setFirstName("Mesut");
@@ -25,6 +30,15 @@ public class StudentRestController {
         students.add(student);
         students.add(student2);
 
+    }
+
+    @RequestMapping("/students")
+    public List<Student> getAllStudents(){
         return students;
+    }
+
+    @RequestMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        return students.get(studentId);
     }
 }
